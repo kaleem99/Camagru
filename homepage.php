520678de000b1e5c4a6ee5917121aa872a7a2051
kmohamm <50695258+kmohamm@users.noreply.github.com>
2019-11-05 14:51:05 +0200

    Add files via upload

---
+++ homepage.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+<head>
+<title>Home</title>
+<link rel="stylesheet" type="text/css" href="homepage.css">
+</head>
+<body>
+
+<div class="home">
+
+    <div class="camera">
+
+    </div>
+</div>
+</body>
+</html>
