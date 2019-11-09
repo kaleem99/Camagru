242a14c0e3cea175b81dffc618af1db000d58edb
kmohamm <50695258+kmohamm@users.noreply.github.com>
2019-11-09 15:32:41 +0200

    Add files via upload

---
+++ SnapShot.php
@@ -0,0 +1,59 @@
+<!doctype html>
+<html lang=''>
+<head>
+   <meta charset='utf-8'>
+   <meta http-equiv="X-UA-Compatible" content="IE=edge">
+   <meta name="viewport" content="width=device-width, initial-scale=1">
+   <link rel="stylesheet" href="SnapShot.css">
+   <script src="script.js" type="text/javascript"></script>
+   <title>SnapShot</title>
+</head>
+<body class="news">
+    <header>
+        <div class="nav">
+            <ul>
+                <li class="home"><a href="homepage.php">Home</a></li>
+                <li class="profile"><a href="profile.php">Profile</a></li>
+                <li class="gallery"><a href="#">Gallery</a></li>
+                <li class="SnapShot"><a class="active">SnapShot</a></li>
+                <li class="logout"><a href="sign.php">Logout</a></li>
+            </ul>
+        </div>
+        <script src="script.js"></script>
+        <div id="newImages">
+            <div>
+                <video id="player">Video is loading...</video>
+			</div>
+			<div >
+				<canvas name="image" id="canvas" width="520px" height="340px">Canvas Still Loading</canvas>
+				<canvas name="image" id="player">Canvas still loading</canvas>
+                <input type="submit" class="button1" value="SnapShot" id="capture">
+            </div>
+  </header>
+  <div id="container">
+<video autoplay = "true" id = "videoElement">
+</video>
+</div>
+<script type="text/javascript">
+    var video = document.querySelector("#videoElement");
+    navigator.getUserMedia=navigator.getUserMedia||navigator.webkitGetUsermedia||
+    navigator.mozGetUserMedia||navigator.msGetUserMedia||navigator.oGetUserMedia;
+    if (navigator.getUserMedia) 
+    {
+        navigator.getUserMedia({video:true}, handleVideo, videoError);
+
+    }
+    function handleVideo(localStream)
+    {
+        self.video.srcObject = localStream;
+    }    
+    function videoError(e)
+    {
+
+    }
+    var context = canvas.getContext('2d');
+    capture.addEventListener("click", function(){
+    context.drawImage(video, 0, 0, 400, 400);});
+</script>
+</body>
+<html>
--- homepage.php
+++ homepage.php
@@ -1,16 +1,23 @@
-<!DOCTYPE html>
-<html>
+<!doctype html>
+<html lang=''>
 <head>
-<title>Home</title>
-<link rel="stylesheet" type="text/css" href="homepage.css">
+   <meta charset='utf-8'>
+   <meta http-equiv="X-UA-Compatible" content="IE=edge">
+   <meta name="viewport" content="width=device-width, initial-scale=1">
+   <link rel="stylesheet" href="homepage.css">
+   <script src="http://code.jquery.com/jquery-latest.min.js" type="text/javascript"></script>
+   <script src="script.js"></script>
+   <title>Homepage</title>
 </head>
 <body>
-
-<div class="home">
-
-    <div class="camera">
-
+    <div class="nav">
+      <ul>
+        <li class="home"><a class="active" href="#">Home</a></li>
+        <li class="profile"><a href="profile.php">Profile</a></li>
+        <li class="gallery"><a href="#">Gallery</a></li>
+        <li class="SnapShot"><a href="SnapShot.php">SnapShot</a></li>
+        <li class="logout"><a href="sign.php">Logout</a></li>
+      </ul>
     </div>
-</div>
 </body>
-</html>
+<html>
